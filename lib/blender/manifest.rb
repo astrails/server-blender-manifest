f248644ed26cabb2a4a70ea6e80f8131b6f7e7b1
Michael Mazyar <taasaa@gmail.com>
2010-06-17 02:33:56 +0300

    Blender::Manifest.run

--- lib/blender/manifest.rb
+++ lib/blender/manifest.rb
@@ -1,5 +1,11 @@
 module Blender
   module Manifest
     ROOT = File.expand_path("../manifest/root.rb", __FILE__)
+
+    # run the root manifest with shadow_puppet of the given VERSION
+    # @param [String] shadow_puppet_version the version of the shadow_puppet gem to use
+    def self.run(shadow_puppet_version)
+      system "shadow_puppet", "_#{shadow_puppet_version}_", ROOT
+    end
   end
 end
